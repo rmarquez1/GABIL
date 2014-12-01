3aefeee19e4c4d841a07c15bc66ae700635152f5
ramon <ramon@localhost.localdomain>
2014-11-30 22:13:12 -0430

    Listo Gabil

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 GABIL
 =====
+Para correr el programa:
+Utilice: 
+	python gabil.py iris.data tasaMutacion tasaCrossover tipoSeleccion penalizacion
+Donde:
+	* tipoSeleccion puede tomar los valores:
+		1 para Rueda de Ruleta
+		2 para Otro
+
+	* penalizacion puede tomar los valores: 
+		1 sin penalización
+		2 con penalización
--- gabil.py
+++ gabil.py
@@ -1,263 +1,161 @@
+from pyevolve import G1DList, G1DBinaryString
+from pyevolve import Mutators, Initializators, Crossovers
+from pyevolve import GSimpleGA
+from pyevolve import Selectors
+from pyevolve import Statistics
+from pyevolve import DBAdapters
+
 import sys
+import pyevolve.Consts
+import pyevolve.Util
 import random
 import numpy as np
 
 
-# Para cada individual calculamos la propabilidad de que este sea escogido
-def prob_selection_population(population):
-    pop_aux = []
-    # Calculamos la suma de todos fitness de la poblacion
-    sum_fitness_total = 0.0
-    for individual in population:
-        sum_fitness_total += individual["fitness"]
-    #print "Sum fitness: ", sum_fitness_total
+population = []
 
-    for individual in population:
-    	if sum_fitness_total != 0.0:
-        	individual["propability"] = individual["fitness"] / sum_fitness_total
-        else: 
-        	print "DIVISION POR CERO"
-        	exit()
-        pop_aux.append(individual)
-
-    population = pop_aux
-    return population
-
-
-# Realizamos la seleccion de los padres
-def selection(population, num_hyp):
-	population = prob_selection_population(population)
-	prob = np.random.uniform(0,1)
-	pop_aux = []
-	k = 0
-	for individual in population:
-		if prob >= 0 and prob <= individual["probability"]:
-			pop_aux.append(individual)
-			k += 1
-		if k == num_hyp:
-			break
-	return pop_aux
-
-
-def selection_random_hyp(population, n):
-	type1 = []
-	type2 = []
-	type3 = []
-	pop_aux = []
-	maxi = n/3
-
-	for i in range(maxi):
-		rd = np.random.uniform(0,1)
-		if rd >= 0 and rd <= 0.33:
-			k = np.random.random_integers(0,15)
-			pop_aux.append(population[k])
-		elif rd > 0.33 and rd <= 0.66:
-			k = np.random.random_integers(16,31)
-			pop_aux.append(population[k])
-		elif rd > 0.66:
-			k = np.random.random_integers(32,49)
-			pop_aux.append(population[k])
-
-	for i in range(maxi):
-		rd = np.random.uniform(0,1)
-		if rd <= 0.33:
-			k = np.random.random_integers(50,65)
-			pop_aux.append(population[k])
-		elif rd > 0.33 and rd <= 0.66:
-			k = np.random.random_integers(66,81)
-			pop_aux.append(population[k])
-		elif rd > 0.66:
-			k = np.random.random_integers(82,99)
-			pop_aux.append(population[k])
-
-	for i in range(maxi):
-		rd = np.random.uniform(0,1)
-		if rd <= 0.33:
-			k = np.random.random_integers(100,115)
-			pop_aux.append(population[k])
-		elif rd > 0.33 and rd <= 0.66:
-			k = np.random.random_integers(116,131)
-			pop_aux.append(population[k])
-		elif rd > 0.66:
-			k = np.random.random_integers(132,149)
-			pop_aux.append(population[k])
-
-	return pop_aux
-
-
-# Generamos 100 individuals aleatoriamente
-def generating_individuals():
-	individuals = []
-	# Generamos 100 individuals
-	for i in range(100):
-		child = []
-		while True:
-			# Generamos un numero del 1 al 100. VERIFICAR ESTO!!!!!!!
-			# Lanzamos dos veces el dado
-			for x in range(2): 
-				n = np.random.random_integers(1,100)
-			if n % 30 == 0:
-				for x in range(n):
-					bit = np.random.random_integers(0,1)
-					child.append(bit)
-				break
-		individuals.append({"individual": child, "fitness": 0.0, "probability": 0.0})
-	return individuals
-
-
-# Operados de cruce
-def crossover_aux(parent_1, parent_2, pc):
-    cross = np.random.uniform(0,1)
-    if cross >= 0 and cross <= pc:
-    	# VERIFICAR ESTOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!!!!!!!!!!!!
-        while True:
-            crossover_index1_p1 = np.random.random_integers(1, len(parent_1))
-            crossover_index2_p1 = np.random.random_integers(1, len(parent_2))
-            if crossover_index2_p1 > crossover_index1_p1:
+def match(genome,parent):
+    tam_genome = len(genome)
+    num_rules = tam_genome / 30
+    rules = []
+    well = 0.0
+    for x in range(num_rules):
+        rule = []
+        cont = x * 30
+        while cont < tam_genome:
+            rule.append(genome[cont])
+            cont += 1
+            if cont % 30 ==0 :
                 break
-         
-        child_1 = parent_1
-        child_2 = parent_2
-        child_1["individual"] = parent_1["individual"][:crossover_index1_p1] + parent_2["individual"][crossover_index1_p1:crossover_index2_p1]+ parent_1["individual"][crossover_index2_p1:]
-        child_2["individual"] = parent_2["individual"][:crossover_index1_p1] + parent_1["individual"][crossover_index1_p1:crossover_index2_p1]+ parent_2["individual"][crossover_index2_p1:]
-        #print crossover_index1_p1
-        #print crossover_index2_p1
-    else:
-        child_1 = parent_1
-        child_2 = parent_2
+        rules.append(rule)
+    ver = 0
+    for rule in rules:
+        for x in range(len(rule)):
+            if (parent[x] == 1) and (rule[x] != 1):
+                ver += 1
+                break
+            else:
+                if (rule[len(rule)-2] == 1) and (rule[len(rule)-1] == 0):
+                    ver+=1
 
-   
-    #print "padre1: ", parent_1
-    #print "padre2: ", parent_2
-    #print "hijo1:  ", child_1
-    #print "hijo2:  ", child_2
-    return [child_1, child_2]
-
-def crossover(population, num_cross, pc):
-	offsprings = []
-	for x in range(int(num_cross*2)):
-		index1 = np.random.random_integers(0,len(population)-1)
-		index2 = np.random.random_integers(0,len(population)-1)
-		parent_1 = population[index1]
-		parent_2 = population[index2]
-		childs = crossover_aux(parent_1,parent_2, pc)
-		for x in childs:
-			offsprings.append(x)
-	return offsprings
+    if ver != num_rules:
+        well += 1.0
+    return well
 
 
+def eval_func(genome):
+    well = 0.0
+    num_ex = len(population)
+    tam_genome = len(genome)
+    num_rules = tam_genome / 30 
+    for parent in population:
+        
+        rules = []
+        for x in range(num_rules):
+            rule = []
+            cont = x * 30
+            while cont < tam_genome:
+                rule.append(genome[cont])
+                cont += 1
+                if cont % 30 ==0 :
+                    break
+            rules.append(rule)
+        ver = 0
+        for rule in rules:
+            for x in range(len(rule)):
+                if (parent[x] == 1) and (rule[x] != 1):
+                    ver += 1
+                    break
 
-# Operacion de mutacion
-def mutate(population, pm):
-	for individual in population:
-		#print "Antes: ",  individual
-		i = 0
-		while i < len(individual["individual"]):
-			muta = np.random.uniform(0,1)
-			if muta >= 0 and muta <= pm:
-				if individual["individual"][i] == 0:
-					individual["individual"][i] = 1
-				else:
-					individual["individual"][i] = 0
-			i += 1
-	return population
+        if ver != num_rules:
+            well += 1.0
+    correct = well / num_ex
+    score = correct * correct
+    return score
 
-# Operacion de match
-def match(example, individual):
-	k = 0
-	i = 0
-	ver = []
-	while k < len(individual):
-		while i < len(example):
-			if (example[i] == 0) and (example[i] != individual[k]):
-				ver.append(0)
-				i = 0
-				break
-			else:
-				ver.append(1)
-				i += 1
+def eval_func_1(genome):
+    well = 0.0
+    num_ex = len(population)
+    tam_genome = len(genome)
+    num_rules = tam_genome / 30 
+    for parent in population:
+        
+        rules = []
+        for x in range(num_rules):
+            rule = []
+            cont = x * 30
+            while cont < tam_genome:
+                rule.append(genome[cont])
+                cont += 1
+                if cont % 30 ==0 :
+                    break
+            rules.append(rule)
+        ver = 0
+        for rule in rules:
+            for x in range(len(rule)):
+                if (parent[x] == 1) and (rule[x] != 1):
+                    ver += 1
+                    break
 
-		
+        if ver != num_rules:
+            well += 1.0
+    correct = well / num_ex
+    score = correct * correct
+    return score/float(tam_genome)
 
-# Funcion de adaptacion (fitness)
-def fitness(individual, num_ex, parents):
-	well = 0.0
-	#print "Fitness anterior: ", individual["fitness"]
-	for parent in parents:
-		check =  match(parent,individual["individual"])
-		#print check
-		if check == True:
-			well += 1.0
-	correct = well / num_ex	
-	#print "well: ", well
-	individual["fitness"] = individual["fitness"] + (correct*correct)
-	#print "Fitness Final: ", individual["fitness"]
+def crossoverGabil(genome, **args):
+    sister = None
+    brother = None
+    gMom = args["mom"]
+    gDad = args["dad"]
+    tamReglas = 30
 
 
-def max_fitness(population):
-	# Almacenamos todos los valores de los fitness
-	val_fitness = []
-	for individual in population:
-		val_fitness.append(individual["fitness"])
-	# Ordenamos de menos a mayor
-	val_fitness.sort()
-	# Ordenamos el diccionario de poblaciones de acuerdo al val_fitness
-	population.sort(key = lambda x:val_fitness.index(x["fitness"]))
-	return population[-1]
+    cuts = [random.randint(0, (len(gMom)-1)/ tamReglas) , random.randint(0, (len(gMom)-1)/tamReglas)]
 
-def GABIL(population, umbral, p,r,m):
-	# Generamos los individuals aleatorios
-	individuals = generating_individuals()
-	# Seleccionamos las muestras del conjunto de datos
-	#examples = selection_random_hyp(population, 60)
-	examples = population
-	# Calculamos los fitness iniciales
-	for individual in individuals:
-		fitness(individual, 150, examples)
-		#print individual["fitness"]
-	
-	# Calculamos probabilidades iniciales
-	#individuals = prob_selection_population(individuals)
-	
-	iterations = 0
-	while iterations < 1000:
-		#print "ITERATION: ", iterations
-		pop_aux1 = selection(individuals, (1-r)*p)
-		pop_aux1 = pop_aux1 + crossover(individuals, (r*p)/2,0.6)
-		pop_aux1 = mutate(pop_aux1, m)
-		individuals = pop_aux1
-		#print "Individuals: ", individuals
-		for individual in individuals:
-			fitness(individual,150,examples)
-		iterations += 1
-	maxi = max_fitness(individuals)
-	print maxi
-	#print individuals
-	suma = 0.0
-	for ex in examples:
-		ver = match(ex, maxi["individual"])
-		print ver
-		if ver == True:
-			suma += 1.0
-			print suma
-	print "TAM: ", len(examples)
-	print "SUMA: ", suma
-	porcentaje = suma/float(len(examples))
-	print "porcentaje: ", porcentaje
-	return max_fitness(individuals)
-		#print "TAM: ", len(pop_aux)
-	#while max()
-	#for x in pop_aux:
-		#print x["fitness"]
+    if cuts[0] > cuts[1]:
+        pyevolve.Util.listSwapElement(cuts, 0, 1)
+
+    restos = [random.randint(0, tamReglas-1), random.randint(0, tamReglas-1)]
+
+    if restos[0] > restos[1]:
+        pyevolve.Util.listSwapElement(restos, 0, 1)
+
+    cuts2 = [random.randint(0, (len(gDad)-1) / tamReglas), random.randint(0, (len(gDad)-1)/tamReglas)]
+
+    if cuts2[0] > cuts2[1]:
+        pyevolve.Util.listSwapElement(cuts2, 0, 1)
+
+
+    if args["count"] >= 1:
+        sister = gMom.clone()
+        sister.resetStats()
+        sister[cuts[0]*tamReglas+restos[0]:cuts[1]*tamReglas+restos[1]] = \
+        gDad[cuts2[0]*tamReglas+restos[0]:cuts2[1]*tamReglas+restos[1]]
+        sister.stringLength = len(sister.genomeString)
+
+    if args["count"] == 2:
+        brother = gDad.clone()
+        brother.resetStats()
+        brother[cuts2[0]*tamReglas+restos[0]:cuts2[1]*tamReglas+restos[1]] = \
+        gMom[cuts[0]*tamReglas+restos[0]:cuts[1]*tamReglas+restos[1]]
+        brother.stringLength = len(brother.genomeString)
+
+    return (sister, brother)
+
 
 def main():
 
-    population = []
 
 	#Verificamos que el pase de argumentos sea correcto
-    if (len(sys.argv)<= 1):
+    if (len(sys.argv)<= 5):
         print "Utilice:"
-        print "python nombrePrograma nombreArchivo"
+        print "python nombrePrograma nombreArchivo tasaMutacion tasaCrossover tipoSeleccion penalizacion"
+        print "Donde: "
+        print " tipoSeleccion: 1- Rueda de Ruleta"
+        print "                2- Otro"
+        print " penalizacion:  1- Sin penalizacion"
+        print "                2- Con penalizacion"
         sys.exit()
 
     # Verificamos la existencia del archivo
@@ -347,16 +245,44 @@ def main():
         hypothesis = tmp + tmp1 + tmp2 + tmp3 + tmp4
         population.append(hypothesis)
     
+    mutationRate = float(sys.argv[2])
+    crossoverRate = float(sys.argv[3])
+    typeSelector = int(sys.argv[4])
+    penalization = int(sys.argv[5])
+
+
+    genome = G1DBinaryString.G1DBinaryString(30)
+    genome.setParams(rangemin=0, rangemax=1)
+    if penalization == 1:
+        genome.evaluator.set(eval_func)
+    else:
+        genome.evaluator.set(eval_func_1)
+    genome.mutator.set(Mutators.G1DBinaryStringMutatorSwap)
+    genome.crossover.set(crossoverGabil)
     
-    #population = prob_selection_population(population)
-    #print population
-    #print crossover_aux(population[0]["individual"], population[100]["individual"],0.6)
-    #mutate(population, 0.2)
-    #generating_individuals(population)
-    #parents = generating_individuals(population)
-    #pop_aux = selection_random_hyp(population, 60)
-    GABIL(population, 0.05,150,0.6,0.2)
-    #fitness(([1,0,0,1],0.0,0.1), 3, [[1,0,0,1,1], [1,0,0,0]])
-    
+    ga = GSimpleGA.GSimpleGA(genome)
+
+    if typeSelector == 1:
+        ga.selector.set(Selectors.GRouletteWheel)
+    else:
+        ga.selector.set(Selectors.GRankSelector)
+    ga.setGenerations(200)
+    ga.setMutationRate(mutationRate)
+    ga.setCrossoverRate(crossoverRate)
+    ga.terminationCriteria.set(GSimpleGA.ConvergenceCriteria)
+
+
+    ga.evolve(freq_stats=20)
+    best = ga.bestIndividual()
+    print best
+
+    #print FitnessGabilAux(best)
+    #print match([0,1,1,0,1,1], [0,1,0])
+    suma = 0.0
+    for ex in population:
+
+        suma +=  match(best,ex)
+    porcentaje = suma/float(len(population))
+    print "Porcentaje correctamente clasificados: ", porcentaje
 
 if __name__=="__main__":main()
